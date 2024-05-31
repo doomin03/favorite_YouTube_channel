503f38762b07d981e52c5cedd3089d326770465b
doomin03 <111326974+doomin03@users.noreply.github.com>
2024-05-31 14:12:46 +0900

    ㅇㅇ

--- favorite_YouTube_channel/lib/main.dart
+++ favorite_YouTube_channel/lib/main.dart
@@ -38,25 +38,71 @@ class _HomePageState extends State<HomePage> {
     "id": null,
   };
 
+  List<Map<String, dynamic>> videoList = [];
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _performvideos(String id) async {
+    final url = Uri.parse('http://127.0.0.1:5000/youtube/videos?id=$id');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        List<String> titles = jsonResponse["title"];
+        List<String> thumbnails = jsonResponse["img"];
+        List<String> urls = jsonResponse["url"];
+
+        for (int i = 0; i < titles.length; i++) {
+          videoList.add({
+            "title": titles[i],
+            "thumbnail": thumbnails[i],
+            "url": urls[i],
+          });
+        }
+        setState(() {});
+      } else {
+        throw Exception('Failed to load video list');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
   Future<void> _performSearch(String query) async {
     final queryEncoded = Uri.encodeComponent(query);
     final url =
-        Uri.parse('http://127.0.0.1:5000/youtube/search?title=$queryEncoded');
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      final jsonResponse = json.decode(response.body);
-      setState(() {
-        _searchResult = {
-          "name": jsonResponse["name"],
-          "icon": jsonResponse["icon"],
-          "fans": jsonResponse["fans"],
-          "id": jsonResponse["id"],
-        };
-      });
-      print(_searchResult["icon"]);
-    } else {
-      throw Exception('Failed to load search result');
+    Uri.parse('http://127.0.0.1:5000/youtube/search?title=$queryEncoded');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        setState(() {
+          _searchResult = {
+            "name": jsonResponse["name"],
+            "icon": jsonResponse["icon"],
+            "fans": jsonResponse["fans"],
+            "id": jsonResponse["id"],
+          };
+        });
+
+        if (_searchResult["id"] != null) {
+          await _performvideos(_searchResult["id"]);
+        }
+      } else {
+        throw Exception('Failed to load search result');
+      }
+    } catch (e) {
+      print('Error: $e');
     }
+  }
+
+  void openVideoUrl(Map<String, dynamic> videoData) {
+    String? videoUrl = videoData["url"];
+    print("Opening video URL: $videoUrl");
   }
 
   @override
@@ -79,7 +125,6 @@ class _HomePageState extends State<HomePage> {
         ),
         body: TabBarView(
           children: [
-            // 첫 번째 탭
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
@@ -94,40 +139,59 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                      SizedBox(width: 10), // 간격 추가
+                      SizedBox(width: 10),
                       ElevatedButton(
-                        onPressed: () {
+                        onPressed: () async {
                           String query = _searchController.text;
-                          _performSearch(query);
+                          await _performSearch(query);
                         },
                         child: Text('검색'),
                       ),
                     ],
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (_searchResult["icon"] != null)
-                      // 아이콘이 있는 경우 이미지를 표시
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(_searchResult["icon"]!),
-                          radius: 50, // 반지름 설정
-                        ),
-                      Column(
-                        children: [
-                          if (_searchResult["name"] != null)
-                            Text(_searchResult["name"]),
-
-                          if (_searchResult["fans"] != null)
-                            Text(_searchResult["fans"]), // 텍스트를 빈 문자열로 표시하는 텍스트 위젯
-                        ],
-                      )
-                    ],
-                  )
+                  if (_searchResult["icon"] != null)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(_searchResult["icon"]!),
+                        radius: 50,
+                      ),
+                    ),
+                  if (_searchResult["name"] != null)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _searchResult["name"],
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  if (_searchResult["fans"] != null)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _searchResult["fans"],
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  Expanded(
+                    child: ListView.separated(
+                      itemCount: videoList.length,
+                      separatorBuilder: (context, index) => Divider(),
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> videoData = videoList[index];
+                        return ListTile(
+                          leading: Image.network(videoData["img"] as String),
+                          title: Text(videoData["title"] as String),
+                          onTap: () {
+                            openVideoUrl(videoData);
+                          },
+                        );
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
-            // 두 번째 탭
             Container(),
           ],
         ),
